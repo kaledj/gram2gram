7a33c1886e2691a5c5c0efec986383605714200e
David Kale <kaledj2@gmail.com>
2017-09-08 18:09:27 -0400

    Fix typo

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ const server = require('./server');
 const bot = require('./bot');
 const load_envvar = require('./env');
 
-const PORT = load_envvar('PORT');
+const PORT = process.env.PORT || 8080;
 
 // Register default error handler
 server.use((err, req, res, next) => {
   bot.sendMessageToSubscribers(`Something did not go so well my dudes.`);
 });
 
-server.listen(PORT, () => console.log(`Server started.  Listening on:${POST}`));
+server.listen(PORT, () => console.log(`Server started.  Listening on:${PORT}`));
